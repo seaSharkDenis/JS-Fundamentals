22ab7219f17b93260b9b45e146dbf569a16c8600
Denis Ndiritu <denisndiritu1@gmail.com>
2025-05-20 08:18:24 +0300

    Switched from a while loop to a for...of loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,9 @@
 const multiLanguages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 let count = 0;
-while(count<multiLanguages.length){
-    console.log(multiLanguages[count]);
-    count+=1;
+// while(count<multiLanguages.length){
+//     console.log(multiLanguages[count]);
+//     count+=1;
+// }
+for(const element of multiLanguages){
+    console.log(element);
 }
