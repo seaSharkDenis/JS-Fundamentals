041abe83da822c496b140a119b84f6099bed8ebd
Denis Ndiritu <denisndiritu1@gmail.com>
2025-05-20 08:14:33 +0300

    Used a while loop to loop through an array of string and printed the lines in it

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+const multiLanguages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let count = 0;
+while(count<multiLanguages.length){
+    console.log(multiLanguages[count]);
+    count+=1;
+}
