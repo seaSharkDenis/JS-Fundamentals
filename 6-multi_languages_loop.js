49b67b09b8f8ffaf8997f6fd69176b78be08dbdf
Denis Ndiritu <denisndiritu1@gmail.com>
2025-05-20 08:19:09 +0300

    Minor commenting

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 const multiLanguages = ["C is fun", "Python is cool", "JavaScript is amazing"];
-let count = 0;
+// let count = 0;
 // while(count<multiLanguages.length){
 //     console.log(multiLanguages[count]);
 //     count+=1;
