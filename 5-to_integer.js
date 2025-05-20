4c090d828b166e5c9ddfa9d8a479ddcb64ad704c
Denis Ndiritu <denisndiritu1@gmail.com>
2025-05-20 08:05:35 +0300

    JS-Intro: Wrote a script that prints a number is the first argument can be converted to an integer

---
+++ 5-to_integer.js
@@ -0,0 +1,7 @@
+const process = require('process');
+const args = process.argv.slice(2);
+if(isNaN(args[0])){
+    console.log("Not a number");
+}else{
+    console.log(`My number: ${parseInt(input)}`);
+}
