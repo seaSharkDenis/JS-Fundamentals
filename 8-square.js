5fa8133c3a73041fdc70254844ac2aa5f32d11ca
Denis Ndiritu <denisndiritu1@gmail.com>
2025-05-20 09:02:21 +0300

    Wrote a script that prints a square

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+const process = require("process");
+const args = process.argv.slice(2);
+if (isNaN(args[0]) || args[0] === undefined) {
+  console.log("Missing size");
+} else {
+  let i = 0;
+  while (i < parseInt(args[0])) {
+    console.log("x".repeat(args[0]));
+    i++;
+  }
+}
